bd7cc576b55f1942ba331e37e162ca549365f832
SAIRAM <sairam@SAIRAMs-MacBook-Pro.local>
2019-06-10 01:19:40 +0530

    remove secondary scripts

--- colorectal.m
+++
@@ -1,9 +0,0 @@
-img=imread('colon-25.png');
-
-R_d = double(img(:,:,1));
-G_d = double(img(:,:,2));
-B_d = double(img(:,:,3));
-
-blueRatio = uint8(((100 * B_d)./(1+R_d+G_d)) .* (256./(1+B_d+R_d+G_d)));
-imtool(blueRatio)
-s = regionprops(blueRatio,'BoundingBox')
